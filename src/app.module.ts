68d6804331af0c47e3f2b907ffc53325b8abbdfd
Ugochidev <ugochukwuchioma16@gmail.com>
2023-01-31 07:45:35 +0100

    feat:added cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BlogModule } from './blog/blog.module';
 import keys from './config/keys';
 
 @Module({
-  imports: [MongooseModule.forRoot(keys.mongoURI)],
+  imports: [MongooseModule.forRoot(keys.mongoURI), BlogModule],
   controllers: [AppController],
   providers: [AppService],
 })
